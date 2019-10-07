579b89179dcd9a9cbeb172184c7387b4b3191424
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-10-07 13:03:36 -0400

    Added ChocBox

---
+++ ChocBoxEthan/ChocBoxEthan/ChocBoxForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ChocBoxEthan
+{
+    partial class frmChocBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.lblText2 = new System.Windows.Forms.Label();
+            this.lblText3 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText1.Location = new System.Drawing.Point(24, 58);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(477, 25);
+            this.lblText1.TabIndex = 0;
+            this.lblText1.Text = "Enter the # of chocolate boxes that you sold";
+            this.lblText1.Click += new System.EventHandler(this.LblText1_Click);
+            // 
+            // lblText2
+            // 
+            this.lblText2.AutoSize = true;
+            this.lblText2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText2.Location = new System.Drawing.Point(39, 220);
+            this.lblText2.Name = "lblText2";
+            this.lblText2.Size = new System.Drawing.Size(133, 24);
+            this.lblText2.TabIndex = 1;
+            this.lblText2.Text = "Your prize is:";
+            this.lblText2.Click += new System.EventHandler(this.LblText2_Click);
+            // 
+            // lblText3
+            // 
+            this.lblText3.AutoSize = true;
+            this.lblText3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText3.Location = new System.Drawing.Point(241, 220);
+            this.lblText3.Name = "lblText3";
+            this.lblText3.Size = new System.Drawing.Size(51, 25);
+            this.lblText3.TabIndex = 2;
+            this.lblText3.Text = "???";
+            this.lblText3.Click += new System.EventHandler(this.LblText3_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(531, 63);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.TextChanged += new System.EventHandler(this.TxtInput_TextChanged);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(154, 135);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(397, 42);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // frmChocBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblText3);
+            this.Controls.Add(this.lblText2);
+            this.Controls.Add(this.lblText1);
+            this.Name = "frmChocBox";
+            this.Text = "Chocolate Box by Ethan";
+            this.Load += new System.EventHandler(this.FrmChocBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.Label lblText2;
+        private System.Windows.Forms.Label lblText3;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ ChocBoxEthan/ChocBoxEthan/ChocBoxForm.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Created by: Ethan L
+ * Created on: 10/07/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #19 - Chocolate Boxes
+ * This program displays the type of prize the user receives depending on the number of chocolate boxes sold.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChocBoxEthan
+{
+    public partial class frmChocBox : Form
+    {
+        public frmChocBox()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmChocBox_Load(object sender, EventArgs e)
+        {
+            // hide the prize label as soon as the program starts
+            this.lblText3.Hide();
+        }
+
+        private void LblText1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            // make the boxesSold label
+            double boxesSold;
+
+            boxesSold = double.Parse(txtInput.Text);
+
+            // this checks if the student sold more than 20 boxes, if they did, they get a prize
+            if (boxesSold > 20)
+            {
+                this.lblText3.Text = "You get a prize!";
+                this.lblText3.Show();
+            }
+            // this checks if the student sold less than 10 boxes, if they did, they get an honourable mention
+            else if (boxesSold < 10)
+            {
+                this.lblText3.Text = "You get an honourable mention";
+                this.lblText3.Show();
+            }
+            // if they didn't sell more than 20 boxes, or less than 10 boxes, they get a small prize
+            else
+            {
+                this.lblText3.Text = "You get a small prize";
+                this.lblText3.Show();
+            }
+        }
+
+        private void LblText2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblText3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
