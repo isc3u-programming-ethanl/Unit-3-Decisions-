ea3d331764a82ae8d7be4db26912afe81cfd16e8
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-10-18 09:24:57 -0400

    Added PerfectSquaresWithWhileLoop

--- PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoopForm.Designer.cs
+++ PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoopForm.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nupInput = new System.Windows.Forms.NumericUpDown();
+            this.lstPerfectSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nupInput)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(176, 74);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 51);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // nupInput
+            // 
+            this.nupInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nupInput.Location = new System.Drawing.Point(180, 160);
+            this.nupInput.Name = "nupInput";
+            this.nupInput.Size = new System.Drawing.Size(120, 22);
+            this.nupInput.TabIndex = 1;
+            this.nupInput.ValueChanged += new System.EventHandler(this.NupInput_ValueChanged);
+            // 
+            // lstPerfectSquares
+            // 
+            this.lstPerfectSquares.FormattingEnabled = true;
+            this.lstPerfectSquares.Location = new System.Drawing.Point(145, 224);
+            this.lstPerfectSquares.Name = "lstPerfectSquares";
+            this.lstPerfectSquares.Size = new System.Drawing.Size(187, 95);
+            this.lstPerfectSquares.TabIndex = 3;
+            this.lstPerfectSquares.SelectedIndexChanged += new System.EventHandler(this.LstPerfectSquares_SelectedIndexChanged);
             // 
             // frmPerfectSquareWithLoopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(479, 450);
+            this.Controls.Add(this.lstPerfectSquares);
+            this.Controls.Add(this.nupInput);
+            this.Controls.Add(this.btnStart);
             this.Name = "frmPerfectSquareWithLoopForm";
             this.Text = "Perfect Squares With While Loop by Ethan";
+            ((System.ComponentModel.ISupportInitialize)(this.nupInput)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nupInput;
+        private System.Windows.Forms.ListBox lstPerfectSquares;
     }
 }
 
--- PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoopForm.cs
+++ PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoop/PerfectSquaresWithWhileLoopForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Ethan L
+ * Created on: 10/18/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day # - Perfect Squares With While Loop
+ * This program gets the users input, and then calculates all the possible perfect squares for the number that the user put in.
+ * It then displays in the list box all the possible perfect squares below it.
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,61 @@ namespace PerfectSquaresWithWhileLoop
         public frmPerfectSquareWithLoopForm()
         {
             InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            int endingValue;
+            int value;
+            Double squareRootAsDouble;
+            int squareRootAsInteger;
+
+            // set the initial values
+            value = 1;
+
+            // clear all the items from the list box
+            lstPerfectSquares.Items.Clear();
+
+            // get the user's end value selection from the number up-down box
+            endingValue = Convert.ToInt32(this.nupInput.Value);
+
+            // continue stating any perfect squares between the minimum value and the user's selected end value
+            while (value <= endingValue)
+            {
+
+                // take the square root of the value
+                squareRootAsDouble = Math.Sqrt(value);
+
+                // convert the double to an integer
+                squareRootAsInteger = Convert.ToInt32(squareRootAsDouble);
+
+                // the only way for both the decimal and the integer to be equal is if the value was a perfect square
+                if (squareRootAsInteger == squareRootAsDouble)
+                {
+                    this.lstPerfectSquares.Items.Add(value + " is a perfect square");
+                 // this.lstPerfectSquares.SetSelected(numberOfPerfectSquares);
+   
+                }
+                
+                // refreshes the form
+                this.Refresh();
+                // increases the value by 1
+                value++;
+            }
+        }
+    
+ 
+       
+
+        private void NupInput_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LstPerfectSquares_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
