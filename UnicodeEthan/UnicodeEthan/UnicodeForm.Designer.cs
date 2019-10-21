c28fde6ec5ee8bc3593ed967740bfde332675f2d
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-10-21 13:07:20 -0400

    Added Unicode

---
+++ UnicodeEthan/UnicodeEthan/UnicodeForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace UnicodeEthan
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstUnicodes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(174, 76);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(105, 52);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lstUnicodes
+            // 
+            this.lstUnicodes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstUnicodes.FormattingEnabled = true;
+            this.lstUnicodes.ItemHeight = 20;
+            this.lstUnicodes.Location = new System.Drawing.Point(112, 177);
+            this.lstUnicodes.Name = "lstUnicodes";
+            this.lstUnicodes.Size = new System.Drawing.Size(233, 104);
+            this.lstUnicodes.TabIndex = 1;
+            this.lstUnicodes.SelectedIndexChanged += new System.EventHandler(this.LstUnicodes_SelectedIndexChanged);
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 450);
+            this.Controls.Add(this.lstUnicodes);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode by Ethan";
+            this.Load += new System.EventHandler(this.FrmUnicode_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstUnicodes;
+    }
+}
+
---
+++ UnicodeEthan/UnicodeEthan/UnicodeForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeEthan
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmUnicode_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            int counter;
+
+            string alphaValue;
+
+            // clear the items in the list box
+            this.lstUnicodes.Items.Clear();
+
+            // make the counter value to 65, increment it until its less than or equal to 90
+            for (counter = 65; counter <= 90; counter++)
+            {
+                // make the alpha value into a hexadecimal
+                alphaValue = Char.ConvertFromUtf32(counter);
+
+                this.lstUnicodes.Items.Add(alphaValue + " ->" + counter);
+            }
+        }
+
+        private void LstUnicodes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
