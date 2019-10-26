ea2e5f736fc501f93a9d44dbd337c8af2a5b11b1
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-10-26 12:20:43 -0400

    Added NumberGuessingGame

---
+++ NumberGuessingGameEthan/NumberGuessingGameEthan/NumberGuessingGameForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace NumberGuessingGameEthan
+{
+    partial class frmNumberGuessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInput = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.picOutput = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(48, 56);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(359, 25);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Guess an integer between 1 and 10:";
+            this.lblInput.Click += new System.EventHandler(this.LblInput_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(53, 132);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(125, 45);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(413, 57);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 26);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.TextChanged += new System.EventHandler(this.TxtInput_TextChanged);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(240, 142);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(48, 25);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "???";
+            this.lblAnswer.Click += new System.EventHandler(this.LblAnswer_Click);
+            // 
+            // picOutput
+            // 
+            this.picOutput.Image = global::NumberGuessingGameEthan.Properties.Resources.checkmark1;
+            this.picOutput.Location = new System.Drawing.Point(331, 202);
+            this.picOutput.Name = "picOutput";
+            this.picOutput.Size = new System.Drawing.Size(182, 182);
+            this.picOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOutput.TabIndex = 4;
+            this.picOutput.TabStop = false;
+            // 
+            // frmNumberGuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picOutput);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblInput);
+            this.Name = "frmNumberGuessingGame";
+            this.Text = "Number Guessing Game by Ethan";
+            this.Load += new System.EventHandler(this.FrmNumberGuessingGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.PictureBox picOutput;
+    }
+}
+
---
+++ NumberGuessingGameEthan/NumberGuessingGameEthan/NumberGuessingGameForm.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by: Ethan L
+ * Created on: 10/26/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #16 - Number Guessing Game
+ * This program I created a program that asks the user to guess a number between 1 and 10. If they get it right, a question mark is displayed.
+ * If they get it wrong, an X is displayed
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuessingGameEthan
+{
+    public partial class frmNumberGuessingGame : Form
+    {
+
+        // declare global variables and constants
+        const int correctGuess = 4;
+        int userGuess;
+
+        public frmNumberGuessingGame()
+        {
+            InitializeComponent();
+
+            // hide the label initially
+            lblAnswer.Hide();
+        }
+
+        private void LblInput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            // assign the rigged number to the label
+            lblAnswer.Text = Convert.ToString(correctGuess);
+
+            // assign userGuess
+            userGuess = int.Parse(txtInput.Text);
+
+            if (userGuess == correctGuess)
+            {
+                // if you got it right, say "you guessed correctly" and a checkmark will show up
+                this.lblAnswer.Text = "You guessed correctly!";
+                this.picOutput.Image = Properties.Resources.checkmark;
+                // show the answer and picture
+                this.lblAnswer.Show();
+                this.picOutput.Show();
+            }
+            else
+            {
+
+                // if you got it wrong, say "you guessed incorrectly" and a red x will show up
+                this.lblAnswer.Text = "You guessed incorrectly!";
+                this.picOutput.Image = Properties.Resources.red_x;
+                // show the answer and picture
+                this.lblAnswer.Show();
+                this.picOutput.Show();
+            }
+        }
+
+        private void LblAnswer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmNumberGuessingGame_Load(object sender, EventArgs e)
+        {
+            // hide the output text and the picture when the form loads
+            this.lblAnswer.Hide();
+            this.picOutput.Hide();
+        }
+    }
+}
